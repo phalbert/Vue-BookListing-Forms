e207acac60ef4ac898ed157c1e8c1db81c39c696
hpal <ahosea15@gmail.com>
2020-04-14 00:53:06 +0300

    filtering books list

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -4,6 +4,14 @@
     <ul>
       <book-item v-for="book in books" :key="book.id" :book="book"></book-item>
     </ul>
+    <hr />
+    <h2>Filtered Books By Ownership</h2>
+    <select v-model="holding">
+      <option v-for="filter in filters" :key="filter">{{ filter }}</option>
+    </select>
+    <ul>
+      <book-item v-for="book in filteredBooks" :key="book.id" :book="book"></book-item>
+    </ul>
     <br />
     <hr />
     <book-form @addBook="appendBook"></book-form>
@@ -11,6 +19,7 @@
 </template>
 
 <script>
+import _ from "lodash";
 import BookItem from "./BookItem";
 import BookForm from "./BookForm";
 
@@ -24,31 +33,42 @@ export default {
         {
           title: "Self-Reliance",
           author: "Ralph Waldo Emerson",
-          finishedReading: true
+          finishedReading: true,
+          ownership: "borrowed"
         },
         {
           title: "American Gods",
           author: "Neil Gaiman",
-          finishedReading: false
+          finishedReading: false,
+          ownership: "bought"
         },
         {
           title: "Amusing Ourselves to Death",
           author: "Neil Postman",
-          finishedReading: true
+          finishedReading: true,
+          ownership: "borrowed"
         }
-      ]
+      ],
+      filters: ["bought", "borrowed"],
+      holding: "bought"
     };
   },
   components: {
     BookItem,
     BookForm
   },
+  computed: {
+    filteredBooks() {
+      return _.filter(this.books, ["ownership", this.holding])
+    }
+  },
   methods: {
     appendBook(bookData) {
       this.books.push({
         title: bookData.bookTitle,
         author: bookData.bookAuthor,
-        finishedReading: bookData.finishedReading
+        finishedReading: bookData.finishedReading,
+        ownership: bookData.ownership
       });
     }
   }
